a49a184efe1e11df171bf11b06e1725aa2da6037
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-13 15:42:04 +0300

    Add admin row

--- gram/admin.py
+++ gram/admin.py
@@ -7,11 +7,14 @@ from .models import Follow, Image, Profile, Comment
 class ImageAdmin(admin.ModelAdmin):
     filter_horizontal =('likes',)
 
+class UserFollow(admin.ModelAdmin):
+    filter_horizontal =('following','followers')
+
 class FollowAdmin(admin.ModelAdmin):
     filter_horizontal =('following','followers')
 
 
-admin.site.register(Profile)
+admin.site.register(Profile, UserFollow)
 admin.site.register(Follow, FollowAdmin)
 admin.site.register(Image, ImageAdmin)
 admin.site.register(Comment)
