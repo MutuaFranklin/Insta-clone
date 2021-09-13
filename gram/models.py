a9750fcd9157604dc104f784e31a768fd502069e
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-13 15:41:47 +0300

    Models cloudinary

--- gram/models.py
+++ gram/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-
-# from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField
 
 
 
@@ -13,8 +12,8 @@ class Profile(models.Model):
     name = models.CharField(blank=True, max_length=120)
     profile_pic = models.ImageField(upload_to = 'instaPhotos',default='avartar.jpg')
     bio = models.TextField(blank= True)
-    followers=models.ManyToManyField(User,related_name='followers')
-    following=models.ManyToManyField(User,related_name='following')
+    followers=models.ManyToManyField(User,related_name='followers', blank=True)
+    following=models.ManyToManyField(User,related_name='following', blank=True)
     
     def __str__(self):
         return self.user.username
@@ -43,16 +42,19 @@ class Profile(models.Model):
 
 
 class Image(models.Model):
-    # image = CloudinaryField('image')
-    image= models.ImageField(upload_to ='instaPhotos', default='avartar.png')
+    image = CloudinaryField('image')
+    # image= models.ImageField(upload_to ='instaPhotos', default='avartar.png')
     image_name =models.CharField(max_length=100)
     image_caption =models.TextField(blank= True)
-    likes = models.ManyToManyField(Profile, related_name="like", blank= True)
+    likes = models.ManyToManyField(Profile, blank= True)
     posted_by = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='post_user')
     posted_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.image_name
+
+    def total_likes(self):
+        return self.likes.count()
 
     def save_image(self):
         self.save()
@@ -95,8 +97,8 @@ class Comment(models.Model):
 
     
 class Follow(models.Model):
-    following = models.ManyToManyField(Profile, blank= True, related_name='user_following')
-    followers = models.ManyToManyField(Profile, blank= True, related_name='user_followers')
+    following = models.ManyToManyField(User, blank= True, related_name='user_following')
+    followers = models.ManyToManyField(User, blank= True, related_name='user_followers')
 
     
 
